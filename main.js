7a33b6e26e278f1cabd2dd215861fbc9c4270d2b
Hip Chip <hchip6207@gmail.com>
2026-02-20 14:39:59 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 function spawn67() {
   var a67 = document.createElement("img");
+  a67.style.position = "fixed";
   a67.src = "https://static.wikia.nocookie.net/stealabr/images/4/40/Fourtyone.png/revision/latest?cb=20251014024859";
   a67.height = "50";
   a67.width = "50";
   var x = Math.floor(Math.random() * 1000 - 1000);
   var y = Math.floor(Math.random() * 1000 - 1000);
-  a67.top = y;
-  a67.right = x;
+  a67.style.top = y;
+  a67.style.right = x;
   document.body.appendChild(a67);
   setInterval(spawn67, 1);
 }
